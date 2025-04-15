efe7dc514054c4491fadd245a6aa8ffd9bf2b048
Arthur Cavalcante <arthur.ferreira@ccc.ufcg.edu.br>
2025-04-15 19:26:24 -0300

    Implements service running in different process

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
@@ -10,25 +10,25 @@ import android.os.Message
 
 class IncrementService : Service() {
 
-
-    private inner class IncrementHandler(looper: Looper): Handler(looper){
-
+    private inner class IncrementHandler(looper: Looper): Handler(looper) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
             msg.data.getInt("VALUE").also {
-                InterEntityCommunication.valueLiveData.postValue(it + 1)
+                Intent("INCREMENT_VALUE_ACTION").putExtra("VALUE", it + 1).apply {
+                    sendBroadcast(this)
+                }
             }
             stopSelf()
         }
-
     }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent?.getIntExtra("VALUE", -1)?.also { value ->
+        intent?.getIntExtra("VALUE", 1)?.also { value ->
             HandlerThread("IncrementThread").apply {
                 start()
                 IncrementHandler(looper).apply {
                     obtainMessage().apply {
-                        data.putInt("VALUE",value)
+                        data.putInt("VALUE", value)
                         sendMessage(this)
                     }
                 }
@@ -36,7 +36,5 @@ class IncrementService : Service() {
         }
         return START_NOT_STICKY
     }
-
-
     override fun onBind(intent: Intent): IBinder? = null
 }
--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -1,6 +1,10 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +20,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var incrementServiceIntent: Intent
     private var counter = 0
+    private val incrementBroadcastReceive = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            intent?.getIntExtra("VALUE", -1)?.also {value ->
+                counter = value
+                Toast.makeText(this@MainActivity, "You clicked $counter times", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +47,21 @@ class MainActivity : AppCompatActivity() {
                 })
             }
         }
-        InterEntityCommunication.valueLiveData.observe(this) { value ->
-            counter = value
-            Toast.makeText(this, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check the versoin to make it compatible with my version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            registerReceiver(incrementBroadcastReceive, IntentFilter("INCREMENT_VALUE_ACTION"), RECEIVER_EXPORTED)
+        }else {
+            registerReceiver(incrementBroadcastReceive, IntentFilter("INCREMENT_VALUE_ACTION"))
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(incrementBroadcastReceive)
     }
 
     override fun onDestroy() {
